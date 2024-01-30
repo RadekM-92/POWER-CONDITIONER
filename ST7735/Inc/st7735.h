c4bc7f5ac68cd5de7caa561bbc00ed1e356dceb5
Radoslaw Marcinowski <rm92.git@gmail.com>
2024-01-30 20:02:01 +0100

    Changed RES, CS, DC pins definition

--- ST7735/Inc/st7735.h
+++ ST7735/Inc/st7735.h
@@ -16,12 +16,12 @@
 #define ST7735_SPI_PORT hspi1
 extern SPI_HandleTypeDef ST7735_SPI_PORT;
 
-#define ST7735_RES_Pin       GPIO_PIN_7
-#define ST7735_RES_GPIO_Port GPIOC
-#define ST7735_CS_Pin        GPIO_PIN_6
+#define ST7735_RES_Pin       GPIO_PIN_10
+#define ST7735_RES_GPIO_Port GPIOA
+#define ST7735_CS_Pin        GPIO_PIN_4
 #define ST7735_CS_GPIO_Port  GPIOB
-#define ST7735_DC_Pin        GPIO_PIN_9
-#define ST7735_DC_GPIO_Port  GPIOA
+#define ST7735_DC_Pin        GPIO_PIN_5
+#define ST7735_DC_GPIO_Port  GPIOB
 
 // AliExpress/eBay 1.8" display, default orientation
 /*

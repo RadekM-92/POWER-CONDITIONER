0524713a1598128c0f9be95be61a02f70805f268
Radoslaw Marcinowski <rm92.git@gmail.com>
2024-02-08 19:53:05 +0100

    Added ADC_Channels_Mapping_t

--- Measurements/Src/measurements.c
+++ Measurements/Src/measurements.c
@@ -2,6 +2,12 @@
 
 #include "measurements.h"
 
+typedef enum
+{
+    Current = 0U,
+    Voltage_Negative = 1U,
+    Voltage_Positive = 2U
 
+}ADC_Channels_Mapping_t;
 
 Sample_t Samples[SAMPLES_AMOUNT_PER_ONE_PERIOD * AMOUNT_OF_PERIODS][AMOUNT_OF_MEASUREMENT_CHANNELS] = {0};

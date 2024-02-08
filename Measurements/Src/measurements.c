fb66ef2386b37eefbd23f43d1640bfa35b2537ac
Radoslaw Marcinowski <rm92.git@gmail.com>
2024-02-08 20:36:14 +0100

    Added ADC_RawToReal() declaration

--- Measurements/Src/measurements.c
+++ Measurements/Src/measurements.c
@@ -14,3 +14,15 @@ typedef enum
   * @brief  Measurements ADC Samples array
   */
 Sample_t Samples[SAMPLES_AMOUNT_PER_ONE_PERIOD * AMOUNT_OF_PERIODS][AMOUNT_OF_MEASUREMENT_CHANNELS] = {0};
+
+
+
+/**
+  * @brief  Conversion from Raw ADC value to real value
+  * 
+  * @param RawVal Raw value from ADC
+  * @param Res ADC resolution 2^n, ex. 4096
+  * @param Ref ADC reference voltage, ex. 3.3 [V]
+  * @retval Real voltage value on ADC pin, ex. 2.5 [V]
+  */
+float ADC_RawToReal(const Sample_t RawVal, const int16_t Res, const int16_t Ref);
